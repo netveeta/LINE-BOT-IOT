be4820a6ce6b16f1491f30f77ad99a2a8d702e73
jeerapote <jeerapote4@hotmail.com>
2018-06-08 23:12:54 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'U7aafed8fb3ffc8dc26a47b6711eb57a4',
+        'to' => 'Cbba671d3c1043d9d231a951b25edc69b',
         'messages' => [$messages],
       ];
       $post = json_encode($data);

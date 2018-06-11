468b11a728b1d4d82172586e4ee01cbfbd7aa9d5
jeerapote <jeerapote4@hotmail.com>
2018-06-11 14:55:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,6 +29,33 @@ if (!is_null($events['events'])) {
 			getMqttfromlineMsg($Topic,$text);//เรียกฟังชั่นที่ pub.php
 			// Make a POST Request to Messaging API to reply to sender
 			
+			$replyToken = $event['replyToken'];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+		
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                        if($text=="สวัสดี"){
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => "จ่าเฉยยินดีรับใช้ครับ"
+			];
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+                                    }// end if
+			
 		}
 	}
 }

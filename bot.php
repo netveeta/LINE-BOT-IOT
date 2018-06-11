60c703193b0afcd57ef2888f549e0b240723df89
jeerapote <jeerapote4@hotmail.com>
2018-06-11 14:53:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
-$access_token1 = 'vIqVV9lNX5yNkf7r4nm+FFAesNeaypSuYC/OOW9LOiRptDrt0/ELtOJekuhmabamyn5ssrtDODisO/XE2wvauE7MTr1C0xIY84aHbRZRQDRtEojxs7UtkvssK7Y4eS4Xj/r+krB7u9ueoZVAjmOvMwdB04t89/1O/w1cDnyilFU='; 
+
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
@@ -28,29 +28,7 @@ if (!is_null($events['events'])) {
 			$Topic = "NodeMCU1" ;
 			getMqttfromlineMsg($Topic,$text);//เรียกฟังชั่นที่ pub.php
 			// Make a POST Request to Messaging API to reply to sender
-			$url1 = 'https://api.line.me/v2/bot/message/reply';
-			$headers1 = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token1);
-                        if($text=="สวัสดี"){
-			// Build message to reply back
-			$messages1 = [
-				'type' => 'text',
-				'text' => "จ่าเฉยยินดีรับใช้ครับ"
-			];
-			$data1 = [
-				'replyToken' => $replyToken1,
-				'messages' => [$messages1],
-			];
-			$post1 = json_encode($data1);
-			$ch1 = curl_init($url1);
-			curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch1, CURLOPT_POSTFIELDS, $post1);
-			curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers1);
-			curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
-			$result1 = curl_exec($ch1);
-			curl_close($ch1);
-			echo $result1 . "\r\n";
-                        }//end if
+			
 		}
 	}
 }

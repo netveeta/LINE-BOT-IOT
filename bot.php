952ae2f94ad994034d65324854772044eef4500a
jeerapote <jeerapote4@hotmail.com>
2018-06-12 09:42:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@
 $content = file_get_contents('php://input');
 // Parse JSON
 $access_token = 'vIqVV9lNX5yNkf7r4nm+FFAesNeaypSuYC/OOW9LOiRptDrt0/ELtOJekuhmabamyn5ssrtDODisO/XE2wvauE7MTr1C0xIY84aHbRZRQDRtEojxs7UtkvssK7Y4eS4Xj/r+krB7u9ueoZVAjmOvMwdB04t89/1O/w1cDnyilFU=';
-$Gid ='Cbba671d3c1043d9d231a951b25edc69b';
+//$Gid ='Cbba671d3c1043d9d231a951b25edc69b';
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
@@ -42,8 +42,7 @@ if (!is_null($events['events'])) {
 				'text' => "จ่าเฉยยินดีรับใช้ครับ"
 			];
 			$data = [
-				//'replyToken' => $replyToken,
-				'to' => $Gid,
+				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
